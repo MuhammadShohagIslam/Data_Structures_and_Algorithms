3e9d396fa021defb30517739de30931d56a81848
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-07-07 16:24:30 +0300

    added time complexity

--- Tree_Traversal/tree_traversal.js
+++ Tree_Traversal/tree_traversal.js
@@ -1,8 +1,21 @@
 /*
         *** Tree Traversal ***
             => Tree Traversal is the one kind of  processing system through which we can visit all of the trees node.
+            
 
         *** We can Traversing a tree two ways ***
             => Breadth-first Search(Starting at the beginning, then working across the tree)
             => Depth-first Search(Preorder, Postorder, Inorder) -> We just going down like our primary direction is going to the end of the tree vertically and coming back up and breadth first going across.
+*/
+/*
+    *** Which is the better BFS Vs DFS ? ***
+        => if we consider Space complexity, breadth first tree is the best.
+        => if the tree is the very depth, depth first tree is take long space
+    
+    *** Use Case InOrder vs PreOrder vs PostOrder ***
+        => If the we want to get all nodes in a tree in their underlying order, InOrdr Depth first tree is good.
+        => PreOrder is the good , if we try to clone or duplicated tree, we want to store flatted in out
+        => 
+    *** Time Complexity of The Tree Traversal ***
+        => Breadth first and depth first tree time complexity is the same beacuse every one visiting node one time
 */
