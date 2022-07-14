6ba514cc682953a24288e39c784f59f05d85e247
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-07-14 22:03:43 +0300

    solving problem by using sliding window p

--- Problem_Solving_Patterns/sliding-window.js
+++ Problem_Solving_Patterns/sliding-window.js
@@ -45,4 +45,62 @@ function maxSubarraySumWithSlideWindow(array, num) {
     }
     return maxSum;
 }
-console.log(maxSubarraySumWithSlideWindow([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// console.log(maxSubarraySumWithSlideWindow([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+/*
+    Write a function called minSubArrayLen which accepts two parameters-
+    an array of positive integers and positive integer.
+
+    This function should return the minimal length of a contiguoussubarray 
+    of which the sum is greater than or equal to the ingeter passed to the function.
+    If there is not one return 0 instead.
+*/
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while (start < nums.length) {
+        // if current window doesn't add up to the given sum then
+        // move the window to right
+        if (total < sum && end < nums.length) {
+            total += nums[end];
+            end++;
+        } else if (total >= sum) {
+            // if current window adds up to at least the sum given then
+            // we can shrink the window
+            minLen = Math.min(minLen, end - start);
+            total -= nums[start];
+            start++;
+        } else {
+            // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+            break;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen;
+}
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7));
+
+/*
+    Write a function called findLongestSubstring, which accepts a string 
+    and returns the length of the longest substring with all distinct characters.
+*/
+function findLongestSubstring(str) {
+    let longest = 0;
+    let start = 0;
+    let seen = {};
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if(seen[char]){
+           start = Math.max(start, seen[char]) 
+        }
+        // index - beginning of substring + 1 (to include current in count)
+        longest = Math.max(longest, i - start + 1);
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1;
+    }
+    return longest;
+}
+console.log(findLongestSubstring("longestsubstring"));
